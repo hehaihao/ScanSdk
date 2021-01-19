443e7327782913534d665196ed80c15148204fd4
hehaihao <7703619+hehaiao@user.noreply.gitee.com>
2021-01-19 16:14:36 +0800

    add scaner

--- scan_lib/src/main/java/com/xm6leefun/scan_lib/ScanApiActivity.java
+++ scan_lib/src/main/java/com/xm6leefun/scan_lib/ScanApiActivity.java
@@ -1,20 +1,226 @@
 package com.xm6leefun.scan_lib;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+import com.xm6leefun.scan_lib.camera.CameraManager;
+import com.xm6leefun.scan_lib.camera.ExScanActivityHandler;
+import com.xm6leefun.scan_lib.camera.InactivityTimer;
+import com.xm6leefun.scan_lib.camera.ViewfinderView;
+
+import java.io.IOException;
+import java.util.Vector;
 
 /**
  * @Description:
  * @Author: hhh
  * @CreateDate: 2021/1/18 13:43
  */
-public class ScanApiActivity extends Activity {
+public class ScanApiActivity extends Activity implements SurfaceHolder.Callback{
+
+    private ExScanActivityHandler handler;
+    private boolean hasSurface;
+    private Vector<BarcodeFormat> decodeFormats;
+    private String characterSet;
+    private InactivityTimer inactivityTimer;
+    private MediaPlayer mediaPlayer;
+    private boolean playBeep;
+    private static final float BEEP_VOLUME = 0.10f;
+    private boolean vibrate;
+
+    SurfaceView surfaceView;
+    ViewfinderView viewfinderView;
+    ImageView iv_scan_close;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_api);
-        BarcodeFormat barcodeFormat = null;
+        surfaceView = findViewById(R.id.preview_view);
+        viewfinderView = findViewById(R.id.viewfinder_view);
+        iv_scan_close = findViewById(R.id.iv_scan_close);
+        iv_scan_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        mHui = getIntent().getStringExtra(HuiMA);
+        initScan();
     }
+
+
+    private CameraManager cameraManager;
+    public CameraManager getCameraManager() {
+        return cameraManager;
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraManager = new CameraManager(this);
+        initOnResume1();
+    }
+
+    private void initOnResume1() {
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            initCamera(surfaceHolder);
+        } else {
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+        decodeFormats = null;
+        characterSet = null;
+        playBeep = true;
+        AudioManager audioService = (AudioManager) getSystemService(AUDIO_SERVICE);
+        if (audioService.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
+            playBeep = false;
+        }
+//        initBeepSound();
+        vibrate = true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        initOnPause2();
+    }
+
+    private void initOnPause2() {
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+        try {
+            CameraManager.get().closeDriver();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            inactivityTimer.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * When the beep has finished playing, rewind to queue up another one.
+     */
+    private final MediaPlayer.OnCompletionListener beepListener = new MediaPlayer.OnCompletionListener() {
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            mediaPlayer.seekTo(0);
+        }
+    };
+
+    public ViewfinderView getViewfinderView() {
+        return viewfinderView;
+    }
+    public Handler getHandler() {
+        return handler;
+    }
+    public void drawViewfinder() {
+        viewfinderView.drawViewfinder();
+    }
+
+    public  static  final String HuiMA="huima";
+    String mHui=null;
+    /**
+     * 处理扫描结果
+     * @param result
+     * @param barcode
+     */
+    String qrCode =null;
+    String resultScan =null;
+    public void handleDecode(Result result, Bitmap barcode) {
+        inactivityTimer.onActivity();
+        resultScan = result.getText();
+
+    }
+
+    private void reScan() {
+        viewfinderView.isRefresh = true;
+        try {
+            handler.initrequestAutoFocus();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            handler.restartPreviewAndDecode();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initScan() {
+        viewfinderView.isRefresh = true;
+        CameraManager.init(getApplication());
+        hasSurface = false;
+        inactivityTimer = new InactivityTimer(this);
+
+    }
+    private Camera camera = null;
+    private Camera.Parameters parameters = null;
+    public static boolean statusFlag = true;
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        try {
+            CameraManager.get().openDriver(surfaceHolder);
+            camera = CameraManager.get().getC();
+            parameters = camera.getParameters();
+        } catch (IOException ioe) {
+            return;
+        } catch (RuntimeException e) {
+            return;
+        }
+        if (handler == null) {
+            handler = new ExScanActivityHandler(ScanApiActivity.this, decodeFormats, characterSet);
+        }
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+    }
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
+    }
+
+    private static final long VIBRATE_DURATION = 200L;
+    /**
+     * 响铃和震动
+     */
+//    private void playBeepSoundAndVibrate() {
+//        if (playBeep && mediaPlayer != null) {
+//            mediaPlayer.start();
+//        }
+//        if (vibrate) {
+//            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+//            vibrator.vibrate(VIBRATE_DURATION);
+//        }
+//    }
 }
