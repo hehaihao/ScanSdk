1ac1abfd824b7dba967134c4b6dfa07c0ee769d8
hehaihao <7703619+hehaiao@user.noreply.gitee.com>
2021-01-19 10:33:25 +0800

    change sdk

--- scan_lib/src/main/java/com/xm6leefun/scan_lib/ScanApiActivity.java
+++ scan_lib/src/main/java/com/xm6leefun/scan_lib/ScanApiActivity.java
@@ -3,6 +3,8 @@ package com.xm6leefun.scan_lib;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.zxing.BarcodeFormat;
+
 /**
  * @Description:
  * @Author: hhh
@@ -13,5 +15,6 @@ public class ScanApiActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_api);
+        BarcodeFormat barcodeFormat = null;
     }
 }
